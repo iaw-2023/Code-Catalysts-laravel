79b2d536d2dbdb39a79983cd92517569247b94bb
Genaro08 <genaro.08@hotmail.es>
2023-04-21 18:19:59 -0300

    mostrar ligas

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('camisetas','App\Http\Controllers\CamisetaController');
+Route::resource('ligas','App\Http\Controllers\LigaController');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
